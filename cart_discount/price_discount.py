d2f8fd4a64f1480a1efbf4a5f17937a1be6a5d65
Your Name <you@example.com>
2024-09-18 19:40:34 -0500

    completed in class

--- cart_discount/price_discount.py
+++ cart_discount/price_discount.py
@@ -2,7 +2,6 @@ def main():
 
     print(discount([10, 4, 20]))  # Expect this to print 4
     # what other lists might this function be called with?
-    
 
 def discount(item_prices):
     """ Complete this function that returns the discount earned for a list of item prices
@@ -10,7 +9,13 @@ def discount(item_prices):
     Example: if this function is called with a list of [10, 4, 20] then return 4.
     """
 
-    pass  # todo replace this line with your code 
+    if not isinstance(item_prices, list):
+        raise ValueError('This function can only work with lists')
+
+    if len(item_prices) >= 3 :
+        return min(item_prices)
+    else :
+        return 0
 
 
 if __name__ == '__main__':
--- cart_discount/test_discount.py
+++ cart_discount/test_discount.py
@@ -1,5 +1,7 @@
 import unittest 
 from unittest import TestCase
+
+from cart_discount import price_discount
 from price_discount import discount  
 
 class TestDiscount(TestCase):
@@ -9,8 +11,54 @@ class TestDiscount(TestCase):
         expected_discount = 4
         self.assertEqual(expected_discount, discount(prices))
 
-    
-    # TODO more unit tests here. Each test should test one scenario
+    # more unit tests here. Each test should test one scenario
+    def test_list_of_two_prices(self):
+        prices = [1, 4]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_list_of_one_price(self):
+        prices = [4]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    def test_empy_list(self):
+        prices = []
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+
+    # invalid data scenarios
+
+    # 0 is falsy value!
+    def test_zero_in_price_list(self):
+        prices = [0, 4, 10]
+        expected_discount = 0
+        self.assertEqual(expected_discount, discount(prices))
+
+    # none is falsy value!
+    def test_None_for_price(self):
+        prices = None
+        self.fail('finish me')
+
+    def test_string_in_price_list(self):
+        prices = ['a', 'b', 'c']
+        self.fail('finish me')
+
+    def test_string_instead_of_list(self):
+        prices = 'Cat'
+        self.fail('finish me')
+
+    def test_negative_numbers_in_price_list(self):
+        prices = [-5, -2, 7]
+        self.fail('finish me')
+
+    def test_floating_points_in_price_list(self):
+        prices = [6.99, 4.5, 3.72]
+        self.fail('finish me')
+
+    # string of numbers ('1', '2', '3', ...)
+
 
 
 if __name__ == '__main__':
